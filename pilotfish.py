f6e2b7c9fcf183caa6ea00fb33ad9831d1a94c14
Thomas Herden <tomherden@gmail.com>
2020-06-17 22:03:36 -0400

    Alpha background images for Windows (tkinter allegedly supports on that OS)

--- pilotfish.py
+++ pilotfish.py
@@ -1,4 +1,5 @@
 from random import choice, shuffle
+import sys
 
 import PySimpleGUI as sg
 
@@ -9,32 +10,40 @@ shuffle(positions)
 direction = choice(directions)
 position = choice(positions)
 
+if sys.platform.startswith("win"):
+    images = {'left':  {'shark': 'images/win_blueshark_l.png',
+                        'pilot': 'images/win_pilotfish_l.png'},
+              'right': {'shark': 'images/win_blueshark_r.png',
+                        'pilot': 'images/win_pilotfish_r.png'}}
+else:
+    images = {'left':  {'shark': 'images/blueshark_l.png',
+                        'pilot': 'images/pilotfish_l.png'},
+              'right': {'shark': 'images/blueshark_r.png',
+                        'pilot': 'images/pilotfish_r.png'}}
+
+
 def get_images():
     global shark_image, pilot_image
 
-    if direction == 'left':
-        shark_image = 'images/blueshark_l.png'
-        pilot_image = 'images/pilotfish_l.png'
-    elif direction == 'right':
-        shark_image = 'images/blueshark_r.png'
-        pilot_image = 'images/pilotfish_r.png'
+    shark_image = images[direction]['shark']
+    pilot_image = images[direction]['pilot']
+
 
 def update_images():
     get_images()
     shark_win['-SHARK-'].update(shark_image)
     pilot_win['-PILOT-'].update(pilot_image)
 
+
 def position_pilotfish():
-    global shark_x, shark_y
-    shark_x, shark_y = shark_win.CurrentLocation()
     if direction == 'left':
-        if position == 'above': # above and behind left-facting shark
+        if position == 'above': # above and behind left-facing shark
             pilot_x = shark_x + 10
             pilot_y = shark_y + 10
-        elif position == 'below_front': # below and before left-facting shark
+        elif position == 'below_front': # below and before left-facing shark
             pilot_x = (shark_x + 20)
             pilot_y = (shark_y + shark_h - pilot_h - 20)
-        elif position == 'below_rear': # below and behind left-facting shark
+        elif position == 'below_rear': # below and behind left-facing shark
             pilot_x = round(shark_x + shark_w - pilot_w - 35)
             pilot_y = (shark_y + shark_h - pilot_h - 35)
     elif direction == 'right':
@@ -53,7 +62,6 @@ def position_pilotfish():
     pilot_win.SetAlpha(0)
     pilot_win.move(pilot_x, pilot_y)
     pilot_win.SetAlpha(1)
-
 
 get_images()
 
@@ -83,20 +91,19 @@ pilot_win = sg.Window(
 
 shark_w, shark_h = shark_win.size
 pilot_w, pilot_h = pilot_win.size
+shark_x, shark_y = shark_win.CurrentLocation()
+last_shark_x, last_shark_y = shark_x, shark_y
 
 position_pilotfish()
 
-# shark_x, shark_y = shark_win.CurrentLocation()
-last_shark_x, last_shark_y = shark_x, shark_y
-
 while True:
-    event, values = shark_win.read(timeout=50)
+    event, values = shark_win.read(timeout=100)
     if event == sg.TIMEOUT_KEY:
         shark_x, shark_y = shark_win.CurrentLocation()
         if (shark_x, shark_y) != (last_shark_x, last_shark_y):
             if shark_x > last_shark_x:
                 last_direction = direction = 'right'
-            elif shark_x < last_shark_x:
+            else:
                 last_direction = direction = 'left'
             update_images()
             position_pilotfish()
